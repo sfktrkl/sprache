92e2c16f605dc3ab18cdfe296553449f4b44a59f
Şafak Türkeli <sfktrkl@gmail.com>
2021-08-15 23:46:12 +0300

    Set up ESLint (#17)
    * Install ESLint
      > npm install --save-dev eslint
    
    * Configure ESLint
      > npx eslint --init
      > How would you like to use ESLint? To check syntax, find problems, and enforce code style
      > What type of modules does your project use? JavaScript modules (import/export)
      > Which framework does your project use? React
      > Does your project use TypeScript? Yes
      > Where does your code run? Node
      > How would you like to define a style for your project? Answer questions about your style
      > What format do you want your config file to be in? JavaScript
      > What style of indentation do you use? Spaces
      > What quotes do you use for strings? Single
      > What line endings do you use? Windows
      > Do you require semicolons? Yes
    
    * ESLint detailed configurations
      > npm install --save-dev eslint-plugin-react @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-plugin-react-hooks
      > Configure .eslint.js
      > Configure package.json
    
    * Execute ESLint
      > npm run lint
      > npm run lint:fix
    
    * Use ESLint on VSCode
      >  install ESLint extension
      > Modify VSCode configuration
      >  ```
      >   "editor.codeActionsOnSave": {
      >     "source.fixAll.eslint": true
      >   },
      > ```

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  ignorePatterns: ["*.js"],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
