3ccc54ec407de55cfd669d2b8b4c9ef8d6917100
Safak TURKELI <sfktrkl@gmail.com>
2021-08-15 22:13:25 +0300

    Set up ESLint
    * Install ESLint
      > npm install --save-dev eslint
    
    * Configure ESLint
      > npx eslint --init
      > How would you like to use ESLint? To check syntax, find problems, and enforce code style
      > What type of modules does your project use? JavaScript modules (import/export)
      > Which framework does your project use? React
      > Does your project use TypeScript? Yes
      > Where does your code run? Node
      > How would you like to define a style for your project? Answer questions about your style
      > What format do you want your config file to be in? JavaScript
      > What style of indentation do you use? Spaces
      > What quotes do you use for strings? Single
      > What line endings do you use? Windows
      > Do you require semicolons? Yes
    
    * ESLint detailed configurations
      > npm install --save-dev eslint-plugin-react @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-plugin-react-hooks
      > Configure .eslint.js
      > Configure package.json
    
    * Execute ESLint
      > npm run lint
      > npm run lint:fix
    
    * Use ESLint on VSCode
      >  install ESLint extension
      > Modify VSCode configuration
      >  ```
      >   "editor.codeActionsOnSave": {
      >     "source.fixAll.eslint": true
      >   },
      > ```

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,14 +1,14 @@
-import React from "react"
-import TestRenderer from "react-test-renderer"
+import React from 'react';
+import TestRenderer, { ReactTestRendererJSON } from 'react-test-renderer';
 
-import App from "../App"
+import App from '../App';
 
 describe('App', function() {
-  const app: any = TestRenderer.create(<App />).toJSON();
-  it("is a View", () => {  // TODO: Router in the future
-    expect(app?.type).toBe("View");
+  const app = TestRenderer.create(<App />).toJSON() as ReactTestRendererJSON;
+  it('is a View', () => {  // TODO: Router in the future
+    expect(app?.type).toBe('View');
   });
-  it("has 1 child", () => {
+  it('has 1 child', () => {
     expect(app?.children?.length).toBe(1);
   });
 });
